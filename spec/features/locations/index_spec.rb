09e0bb798eae80b51feea72f03659d3d12dbc899
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-04 18:26:14 -0500

    Complete test for Location User Story 1

---
+++ spec/features/locations/index_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "locations index page" do
+  it "can see all locations names" do
+    location_1 = Location.create({
+      name: "Philadelphia",
+      population: 15000000,
+      urban: true})
+    location_2 = Location.create({
+      name: "New York City",
+      population: 35000000,
+      urban: true})
+
+    visit "/locations"
+
+    expect(page).to have_content(location_1.name)
+    expect(page).to have_content(location_2.name)
+  end
+end
