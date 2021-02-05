dba08215aabb47da16a0856e66fd8e8463b2678b
Katy La Tour <klatour324@gmail.com>
2021-02-04 21:51:05 -0600

    Add and pass creation spec for Libraries and update new library form input tags with ids

---
+++ spec/features/libraries/creation_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'libraries creation page' do
+  it 'can create a new library' do
+    visit '/libraries'
+
+    expect(page).to have_link('New Library')
+
+    click_link 'New Library'
+
+    expect(current_path).to eq('/libraries/new')
+    expect(page).to have_content('Enter a new Library:')
+
+    fill_in('library_name', with: 'Chicago Public Library')
+    check('A Checkbox')
+    fill_in('years_opened', with: 150)
+
+    click_button 'Submit'
+
+    expect(current_path).to eq('/libraries')
+    expect(page).to have_link('Chicago Public Library')
+  end
+
+end
