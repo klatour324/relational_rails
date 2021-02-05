741896306df63c31e617cd676d0ad9965a9cbf76
Katy La Tour <klatour324@gmail.com>
2021-02-04 18:04:26 -0600

    Create and pass test for libraries user story 2 show page

--- spec/features/libraries/index_spec.rb
+++ spec/features/libraries/index_spec.rb
@@ -16,4 +16,23 @@ RSpec.describe "libraries index page" do
     expect(page).to have_content(libraries_1.name)
     expect(page).to have_content(libraries_2.name)
   end
+
+  describe "libraries individual show page" do
+    it "can show and individual library and its attributes" do
+      libraries_1 = Library.create({
+                  name: "Chicago Public Library",
+                  public: true,
+                  years_opened: 150})
+      libraries_2 = Library.create({
+                  name: "Newberry Library",
+                  public: false,
+                  years_opened: 75})
+
+      visit "/libraries/#{libraries_1.id}"
+
+      expect(page).to have_content(libraries_1.name)
+      expect(page).to have_content(libraries_1.public)
+      expect(page).to have_content(libraries_1.years_opened)
+    end
+  end
 end
