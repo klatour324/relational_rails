0760bd63cc1a7890b1d2787f7b7d82ba60a6c9ff
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-05 10:42:41 -0500

    Complete Bookstore migration and new method

--- app/controllers/bookstores_controller.rb
+++ app/controllers/bookstores_controller.rb
@@ -2,4 +2,21 @@ class BookstoresController < ApplicationController
   def index
     @bookstores = ["books"]
   end
+
+  def new
+  end
+
+  def create
+    binding.pry
+    bookstore = Bookstore.new({
+      name: params[:bookstore][:name],
+      inventory: params[:bookstore][:inventory],
+      open: params[:bookstore][:open],
+      location: self.location
+      })
+
+    bookstore.save
+    binding.pry
+    redirect_to '/bookstores'
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_225658) do
+ActiveRecord::Schema.define(version: 2021_02_05_154030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookstores", force: :cascade do |t|
     t.string "name"
-    t.boolean "open"
     t.integer "inventory"
+    t.boolean "open"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "location_id"
-    t.index ["location_id"], name: "index_bookstores_on_location_id"
   end
 
   create_table "libraries", force: :cascade do |t|
@@ -41,5 +39,4 @@ ActiveRecord::Schema.define(version: 2021_02_04_225658) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "bookstores", "locations"
 end
