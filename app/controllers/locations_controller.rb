67a2673118629b6170acb25817cd518429f72f64
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-05 14:31:52 -0500

    Revise boolean logic to live in view file

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -20,32 +20,24 @@ class LocationsController < ApplicationController
   def show
     @location = Location.find(params[:id])
   end
-  def urban?
-      @location = Location.find(params[:id])
-      if @location == nil
-        false
-      else
-        true
-      end
-    end
 
-    def edit
-      @location = Location.find(params[:id])
-    end
-
-    def update
-      location = Location.find(params[:id])
-      location.update({
-        name: params[:location][:name],
-        population: params[:location][:population],
-        urban: params[:location][:urban]
-        })
-      location.save
-      redirect_to "/locations/#{location.id}"
-    end
-
-    def destroy
-      Location.destroy(params[:id])
-      redirect_to '/locations/'
-    end
+  def edit
+    @location = Location.find(params[:id])
   end
+  
+  def update
+    location = Location.find(params[:id])
+    location.update({
+      name: params[:location][:name],
+      population: params[:location][:population],
+      urban: params[:location][:urban]
+      })
+    location.save
+    redirect_to "/locations/#{location.id}"
+  end
+
+  def destroy
+    Location.destroy(params[:id])
+    redirect_to '/locations/'
+  end
+end
