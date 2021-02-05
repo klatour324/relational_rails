f855a45d02a089c2c67c82a5a1977c090d803357
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-04 20:16:05 -0500

    Complete tests for User Story 2

--- spec/features/locations/index_spec.rb
+++ spec/features/locations/index_spec.rb
@@ -16,4 +16,19 @@ RSpec.describe "locations index page" do
     expect(page).to have_content(location_1.name)
     expect(page).to have_content(location_2.name)
   end
+
+  describe "locations individual location show page" do
+    it "can show all of the location's attributes" do
+      location_1 = Location.create({
+        name: "Philadelphia",
+        population: 15000000,
+        urban: true})
+
+      visit "/locations/#{location_1.id}"
+
+      expect(page).to have_content(location_1.name)
+      expect(page).to have_content(location_1.population)
+      expect(page).to have_content(location_1.urban)
+    end
+  end
 end
