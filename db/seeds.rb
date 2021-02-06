d6b2502a049085fb16111b68a55f864ff69ae726
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-06 11:27:40 -0500

    Add location and bookstore data for rails console dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Location.destroy_all
+Bookstore.destroy_all
+
+philly = Location.create! ({name: "Philadelphia",
+  population: 1500000,urban: true})
+nyc = Location.create! ({name: "New York City",population: 8400000,urban: true})
+mtown = Location.create! ({name: "Middletown",population: 65000,urban: false})
+
+philly.bookstores.create! ({name: "Joseph Fox BookStop",inventory: 444,open: false})
+philly.bookstores.create! ({name: "Harriett's Bookshop",inventory: 3000,open: true})
+
+nyc.bookstores.create! ({name: "Strand Book Store",inventory: 600,open: true})
+nyc.bookstores.create! ({name: "Rizzoli Bookstore",inventory: 470,open: true})
+
+mtown.bookstores.create! ({name: "Barnes and Nobles",inventory: 13000,open: true})
+mtown.bookstores.create! ({name: "River Road Books",inventory: 0,open: false})
