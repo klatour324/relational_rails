90eb0a290b69efdb32df202a0dc7b0d75b50e9da
Katy La Tour <klatour324@gmail.com>
2021-02-07 10:43:33 -0600

    Refactor structure of libraries spec files

--- spec/features/libraries/index_spec.rb
+++ spec/features/libraries/index_spec.rb
@@ -17,5 +17,23 @@ RSpec.describe "libraries index page" do
     expect(page).to have_content(libraries_2.name)
   end
 
+  it 'can create a new library' do
+    visit '/libraries'
 
+    expect(page).to have_link('New Library')
+
+    click_link 'New Library'
+
+    expect(current_path).to eq('/libraries/new')
+    expect(page).to have_content('Enter a new Library:')
+
+    fill_in('library_name', with: 'Chicago Public Library')
+    check('a_checkbox')
+    fill_in('years_opened', with: 150)
+
+    click_button 'create_library'
+
+    expect(current_path).to eq('/libraries')
+    expect(page).to have_link('Chicago Public Library')
+  end
 end
--- spec/features/libraries/show_spec.rb
+++ spec/features/libraries/show_spec.rb
@@ -17,4 +17,43 @@ RSpec.describe "libraries individual show page" do
     expect(page).to have_content(libraries_1.public)
     expect(page).to have_content(libraries_1.years_opened)
   end
+
+  it 'can update information about a library' do
+    libraries_1 = Library.create({
+                name: "Chicago Public Library",
+                public: true,
+                years_opened: 150})
+
+    visit "/libraries/#{libraries_1.id}"
+
+    expect(page).to have_link("Update Library")
+
+    click_link "Update Library"
+
+    expect(current_path).to eq("/libraries/#{libraries_1.id}/edit")
+
+    fill_in('library_name', with: 'Chicago Public Library')
+    check('a_checkbox')
+    fill_in('years_opened', with: 150)
+
+    click_button "Submit"
+
+    expect(current_path).to eq("/libraries/#{libraries_1.id}")
+  end
+
+  it 'can delete a library' do
+    libraries_1 = Library.create({
+                name: "Chicago Public Library",
+                public: true,
+                years_opened: 150})
+
+    visit "/libraries/#{libraries_1.id}"
+
+    expect(page).to have_link("Delete Library")
+
+    click_link "Delete Library"
+
+    expect(current_path).to eq('/libraries')
+    expect(page).to have_no_content("Chicago Public Library")
+  end
 end
