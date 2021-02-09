80b99a5ff0ea0ed0ec4e813c5de13440fbf657e8
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-09 18:26:15 -0500

    Refactor locations/edit_spec to capture boolean edge cases

--- spec/features/locations/edit_spec.rb
+++ spec/features/locations/edit_spec.rb
@@ -7,11 +7,7 @@ RSpec.describe "locations individual instance edit page" do
       population: 16000000,
       urban: true})
 
-    visit "/locations/#{location_1.id}"
-    click_on("Edit Location")
-
-    expect(page).to have_content(location_1.name)
-    expect(page).to have_content("Update information")
+    visit "/locations/#{location_1.id}/edit"
 
     fill_in("loc_name", with: "Philadelphia")
     fill_in("loc_pop", with: (location_1.population + 500))
@@ -19,13 +15,12 @@ RSpec.describe "locations individual instance edit page" do
 
     click_on("submit")
 
-    expect(page).to have_content(location_1.name)
     expect(page).to have_content("Philadelphia")
-    expect(page).to have_content(160005000)
+    expect(page).to have_content(location_1.population + 500)
     expect(page).to have_content(true)
   end
 
-  it "can update a location boolean" do
+  it "can update a location boolean beginning from true" do
     location_1 = Location.create({
       name: "Philadelphia",
       population: 16000000,
@@ -33,13 +28,33 @@ RSpec.describe "locations individual instance edit page" do
 
     visit "/locations/#{location_1.id}/edit"
     uncheck('urban?')
-
     click_on("submit")
+
     expect(page).to have_content(false)
 
     visit "/locations/#{location_1.id}/edit"
     check('urban?')
+    click_on("submit")
 
     expect(page).to have_content(true)
+  end
+
+  it "can update a location boolean beginning from false" do
+    location_2 = Location.create({
+      name: "Mars",
+      population: 0,
+      urban: false})
+
+    visit "/locations/#{location_2.id}/edit"
+    check('urban?')
+    click_on("submit")
+
+    expect(page).to have_content(true)
+
+    visit "/locations/#{location_2.id}/edit"
+    uncheck('urban?')
+    click_on("submit")
+
+    expect(page).to have_content(false)
   end
 end
