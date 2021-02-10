b594f9e8839cd6d15023b019b0490e93f8fe4538
Katy La Tour <klatour324@gmail.com>
2021-02-09 18:24:02 -0600

    Refactor naming of variables, styling, and add user story 18 feature spec

--- spec/features/libraries/index_spec.rb
+++ spec/features/libraries/index_spec.rb
@@ -1,41 +1,67 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "libraries index page" do
   it "can see all libraries names with date and time created at" do
-    libraries_1 = Library.create({
+    library_1 = Library.create!({
                 name: "Chicago Public Library",
                 public: true,
                 years_opened: 150})
-    libraries_2 = Library.create({
+    library_2 = Library.create!({
                 name: "Newberry Library",
                 public: false,
                 years_opened: 75})
 
     visit "/libraries"
 
-    expect(page).to have_content(libraries_1.name)
-    expect(page).to have_content(libraries_1.created_at)
-    expect(page).to have_content(libraries_2.name)
-    expect(page).to have_content(libraries_2.created_at)
+    expect(page).to have_content(library_1.name)
+    expect(page).to have_content(library_1.created_at)
+    expect(page).to have_content(library_2.name)
+    expect(page).to have_content(library_2.created_at)
   end
 
-  it 'can create a new library' do
-    visit '/libraries'
+  it "can create a new library" do
+    visit "/libraries"
 
-    expect(page).to have_link('New Library')
+    expect(page).to have_link("New Library")
 
-    click_link 'New Library'
+    click_link "New Library"
 
-    expect(current_path).to eq('/libraries/new')
-    expect(page).to have_content('Enter a new Library:')
+    expect(current_path).to eq("/libraries/new")
+    expect(page).to have_content("Enter a new Library:")
 
-    fill_in('library_name', with: 'Chicago Public Library')
-    check('a_checkbox')
-    fill_in('years_opened', with: 150)
+    fill_in("library_name", with: "Chicago Public Library")
+    check("a_checkbox")
+    fill_in("years_opened", with: 150)
 
-    click_button 'create_library'
+    click_button "create_library"
 
-    expect(current_path).to eq('/libraries')
-    expect(page).to have_link('Chicago Public Library')
+    expect(current_path).to eq("/libraries")
+    expect(page).to have_content("Chicago Public Library")
+  end
+
+  it "can update a library through individual edit link per library" do
+    library_1 = Library.create!({
+                name: "Chicago Public Library",
+                public: true,
+                years_opened: 150})
+    library_2 = Library.create!({
+                name: "Newberry Library",
+                public: false,
+                years_opened: 75})
+    library_3 = Library.create!({
+                name: "Hollywood Hills Library",
+                public: false,
+                years_opened: 75})
+
+    visit "/libraries"
+
+    expect(page).to have_link("Update Library")
+    expect(page).to have_content("Chicago Public Library")
+    expect(page).to have_content("Newberry Library")
+    expect(page).to have_content("Hollywood Hills Library")
+
+    click_link("Update Library", match: :first)
+
+    expect(current_path).to eq("/libraries/#{library_3.id}/edit")
   end
 end
