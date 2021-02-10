432d99eb53985f41088d5103437373a5845d7601
Katy La Tour <klatour324@gmail.com>
2021-02-10 17:44:54 -0600

    Add feature specs that test for boolean checkbox edge cases

--- spec/features/libraries_books/library_books_creation_spec.rb
+++ spec/features/libraries_books/library_books_creation_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe "library_books creation" do
     library_1 = Library.create!(name: "Chicago Public Library",
                                 public: true,
                                 years_opened: 150)
+    book_1 = library_1.books.create!(title: "To Kill a Mockingbird",
+                                      checked_out: true,
+                                      pages: 487)
+    book_2 = library_1.books.create!(title: "The Handmaid's Tale",
+                                      checked_out: false,
+                                      pages: 227)
 
     visit "/libraries/#{library_1.id}/books"
 
@@ -23,6 +29,68 @@ RSpec.describe "library_books creation" do
 
     expect(current_path).to eq("/libraries/#{library_1.id}/books")
     expect(page).to have_content("A Mind for Numbers")
+  end
+
+  it "can see if a book is checked out from the new book created" do
+    library_1 = Library.create!(name: "Chicago Public Library",
+                                public: true,
+                                years_opened: 150)
+    book_1 = library_1.books.create!(title: "To Kill a Mockingbird",
+                                      checked_out: true,
+                                      pages: 487)
+    book_2 = library_1.books.create!(title: "The Handmaid's Tale",
+                                      checked_out: false,
+                                      pages: 227)
+
+    visit "/libraries/#{library_1.id}/books"
+
+    expect(page).to have_link("Create Book")
+
+    click_link "Create Book"
+
+    expect(current_path).to eq("/libraries/#{library_1.id}/books/new")
+    expect(page).to have_content("Add a New Book")
+
+    fill_in("book_title", with: "A Mind for Numbers")
+    check("a_checkbox")
+    fill_in("pages", with: 267)
+
+    click_button "Create Book"
+
+    expect(current_path).to eq("/libraries/#{library_1.id}/books")
+    expect(page).to have_content(true)
+    expect(page).to have_content("A Mind for Numbers")
+  end
+
+  it "can see if a book has not been checked out" do
+    library_1 = Library.create!(name: "Chicago Public Library",
+                                public: true,
+                                years_opened: 150)
+    book_1 = library_1.books.create!(title: "To Kill a Mockingbird",
+                                      checked_out: true,
+                                      pages: 487)
+    book_2 = library_1.books.create!(title: "The Handmaid's Tale",
+                                      checked_out: false,
+                                      pages: 227)
+
+    visit "/libraries/#{library_1.id}/books"
+
+    expect(page).to have_link("Create Book")
+
+    click_link "Create Book"
+
+    expect(current_path).to eq("/libraries/#{library_1.id}/books/new")
+    expect(page).to have_content("Add a New Book")
+
+    fill_in("book_title", with: "Milkweed")
+    uncheck("a_checkbox")
+    fill_in("pages", with: 300)
+
+    click_button "Create Book"
+
+    expect(current_path).to eq("/libraries/#{library_1.id}/books")
+    expect(page).to have_content(false)
+    expect(page).to have_content("Milkweed")
   end
 
   it "can display links to books index page and library index page" do
