b672c67ff244b9f07255ef26d65dfbd9803790f4
Katy La Tour <klatour324@gmail.com>
2021-02-10 18:29:34 -0600

    Add strong params to libraries and library_books_controller

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,17 +1,13 @@
 class LibrariesController < ApplicationController
   def index
-    @libraries = Library.all
     @libraries = Library.all.order_by_created_date
   end
 
   def create
-    library = Library.new({
-      name: params[:library][:name],
-      public: params[:library][:public],
-      years_opened: params[:library][:years_opened]
-      })
+    library = Library.new(library_params)
 
     library.save
+
     redirect_to '/libraries'
   end
 
@@ -37,5 +33,11 @@ class LibrariesController < ApplicationController
   def destroy
     Library.destroy(params[:id])
     redirect_to '/libraries'
+  end
+
+  private
+
+  def library_params
+    params.require(:library).permit(:name, :public, :years_opened)
   end
 end
--- app/controllers/library_books_controller.rb
+++ app/controllers/library_books_controller.rb
@@ -16,12 +16,16 @@ class LibraryBooksController < ApplicationController
 
   def create
     library = Library.find(params[:library_id])
-    book = library.books.create!({
-      title: params[:book][:title],
-      checked_out: params[:book][:checked_out],
-      pages: params[:book][:pages]
-      })
+    book = library.books.new(book_params)
+
+    book.save
 
     redirect_to "/libraries/#{library.id}/books"
+  end
+
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :checked_out, :pages)
   end
 end
